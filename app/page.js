18bbe7231444563320b9ba4008fab20dcd2cb299
Sophia Zhang <sophia.p.zhang@gmail.com>
2024-10-20 09:39:00 -0700

    page layout

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import EEGMusicInterface from './EEGMusicInterface';
-import BrainAnim from "./brain-anim/BrainAnim"
+import EEGMusicInterface from "./EEGMusicInterface";
+import BrainAnim from "./brain-anim/BrainAnim";
 
 export default function Home() {
   return (
-    <div style={{backgroundColor: 'black'}}>
+    <div style={{ backgroundColor: "black" }}>
       <BrainAnim></BrainAnim>
       <EEGMusicInterface></EEGMusicInterface>
     </div>
